b13f28a7a49c2b29d76a3f0c849df88420a2e373
Victor Danell <victor.danell@gmail.com>
2013-11-14 13:27:16 +0100

    Cleanup js

--- js/questions.js
+++ js/questions.js
@@ -1,8 +1,9 @@
 window.backendURL = 'http://130.240.5.168:5000';
 
 var Question = function(data) {
+   this.author    = ko.observable();
    this.id        = ko.observable();
-   this.body      = ko.observable("Loading question...");
+   this.body      = ko.observable("Body placeholder");
    this.title     = ko.observable("Title placeholder");
    this.tags      = ko.observable();
    this.timestamp = ko.observable("undefined time");
@@ -12,7 +13,7 @@ var Question = function(data) {
    this.deleteQuestion = function() {
       var qid = self.id();
       $.ajax({
-        url: 'http://130.240.5.168:5000/questions/' + qid + '/',
+        url: window.backendURL + '/questions/' + qid + '/',
         type: 'DELETE',
         success: function(result) {
             // Do something with the result
@@ -24,6 +25,7 @@ var Question = function(data) {
 };
 
 Question.prototype.update = function(data) {
+   this.author(data.author)
    this.id(data.id);
    this.body(data.body);
    this.title(data.title);
@@ -33,16 +35,9 @@ Question.prototype.update = function(data) {
 };
 
 var QuestionViewModel = function() {
-   this.questions = ko.observableArray();
-   this.isViewingQuestion = function() { return (questionViewModel.viewedQuestion() != undefined); }
+   this.questions      = ko.observableArray();
    this.viewedQuestion = ko.observable();
-   this.viewQuestion = this.viewQuestion.bind(this);
-
-   /*for (var i=0;i<5;i++)
-   { 
-      var q = {id:i, body: "look at that body", title: "Is this dummy question #" + i + "?", tags: "#demo #2013 #notreallyaquestion", timestamp: 'timestamp 2013'};
-      this.questions.push(new Question(q));
-   }*/
+   this.viewQuestion   = this.viewQuestion.bind(this);
 
    self = this;
    $.getJSON(window.backendURL + '/questions/').done(function(data) {
@@ -53,69 +48,46 @@ var QuestionViewModel = function() {
       }
    });
 
-    
+  // Establish Variables
+  var State = History.getState();
+
+  // Bind to State Change
+  History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+     var State = History.getState(); // Note: We are using History.getState() instead of event.state
+     //History.log('statechange:', State.data, State.title, State.url);
+     //History.log('questionId: ', State.data.state);
+     self.viewedQuestion(self.getQuestionById(State.data.state)[0]);
+  });
 };
 
 ko.utils.extend(QuestionViewModel.prototype, {
-   viewQuestion: function(question) {
-      var questionId = ko.toJS(question.id);
-   
-      question.replies([]);
-      //question.replies.push({body: 'random reply', id: Math.random(), timestamp: "TIMESTAMP"});
-      $.getJSON(window.backendURL + "/questions/" + questionId + "/replies/").done(function(data) {
-         question.replies([]);
-         console.log("Loading replies for question: " + questionId);
-         var rl = data.ReplyList;
-         for (var i = 0; i < rl.length; i++) {
-            question.replies.push({				
-            body: ko.observable(rl[i].body),
-            id: ko.observable(rl[i].id),
-            timestamp: ko.observable(rl[i].timestamp)
-           });
-         } 
-      });
+  isViewingQuestion: function() { 
+    return (this.viewedQuestion() != undefined); 
+  },
+  getQuestionById: function(id) {
+    return $.grep(self.questions(), function(q) { return q.id() == id; });
+  },
+  isQuestionSelected: function(id) {
+    return (this.isViewingQuestion() && this.viewedQuestion().id() == id());
+  },
+  viewQuestion: function(question) {
+    var questionId = ko.toJS(question.id);
 
-      History.pushState({state:questionId, rnd:Math.random()}, null, "?question=" + questionId);
-   }
+    question.replies([]);
+    $.getJSON(window.backendURL + "/questions/" + questionId + "/replies/").done(function(data) {
+       question.replies([]);
+       console.log("Loading replies for question: " + questionId);
+       var rl = data.ReplyList;
+       for (var i = 0; i < rl.length; i++) {
+          question.replies.push({				
+          body: ko.observable(rl[i].body),
+          id: ko.observable(rl[i].id),
+          timestamp: ko.observable(rl[i].timestamp)
+         });
+       } 
+    });
+
+    History.pushState({state:questionId, rnd:Math.random()}, null, "?question=" + questionId);
+ }
 });
-
-// Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
-// Could be stored in a separate utility library
-ko.bindingHandlers.fadeVisible = {
-    init: function(element, valueAccessor) {
-        $(element).hide();
-         
-        var value = valueAccessor();
-        ko.unwrap(value) ? $(element).fadeIn() : $(element).fadeOut();
-    },
-    update: function(element, valueAccessor) {
-        $(element).hide();
-        // Whenever the value subsequently changes, slowly fade the element in or out
-        var value = valueAccessor();
-        ko.unwrap(value) ? $(element).fadeIn() : $(element).fadeOut();
-    }
-};
-
-function getQuestionById(id) {
-   return $.grep(questionViewModel.questions(), function(q) { return q.id() == id; });
-}
-
-function isQuestionSelected(id) {
-   return (questionViewModel.viewedQuestion() != undefined && 
-		questionViewModel.viewedQuestion().id() == id());
-}
-
-// Establish Variables
-var State = History.getState();
-var questionViewModel = new QuestionViewModel();
-
-// Bind to State Change
-History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-   var State = History.getState(); // Note: We are using History.getState() instead of event.state
-   //History.log('statechange:', State.data, State.title, State.url);
-   //History.log('questionId: ', State.data.state);
-
-   questionViewModel.viewedQuestion(getQuestionById(State.data.state)[0]);
-});
-
 
