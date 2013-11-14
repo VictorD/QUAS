16725eaa469217b5c7bf78d909481b8e010e51bc
Thurfjell <ludwig.thurfjell@gmail.com>
2013-11-14 15:57:44 +0100

    Insane profile js magic
    With insane profile html

--- js/profile.js
+++ js/profile.js
@@ -1,16 +1,17 @@
 window.backendURL = 'http://130.240.5.168:5000';
 
-var Profile = function(data) {
+var Profile = function() {
   this.id           = ko.observable();
   this.description  = ko.observable();
-  this.username   = ko.observable();
-  this.update(data);
+  this.username   	= ko.observable();
+  this.create_at	= ko.observable();
 };
 
 Profile.prototype.update = function(data) {
   this.id(data.id);
   this.description(data.description);
   this.username(data.username);
+  this.create_at(data.create_at);
 };
 
 //  src: http://stackoverflow.com/questions/3788125/jquery-querystring
@@ -22,42 +23,42 @@ function querystring(key) {
 }
 
 var ProfileModel = function(){
+	var self = this;
+	var uid = querystring('user');;
   $.support.cors = true;
 
   this.submitUsername = function(){
-    var data = JSON.stringify({
-      username: self.profile().username(),
-      description: self.profile().description()
-    });
-  
+    var data = JSON.stringify(
+		ko.toJS(
+		// send only usr and desc if u want lolz
+			self.profile
+		)
+	);
+	alert("we are in subU");
     postJSON(window.backendURL + '/u/' + uid + '/', 'PUT', data).done(function(response) {
       console.log("POSTED BAD REQUEST?");
-      self.responseJSON(response);
     }); 
   }
 
-  var uid = querystring('user');
+  
 
-  this.profile = ko.observable();
-  this.profile(new Profile({
-    "User": {
-      "create_at": "Wed, 13 Nov 2013 11:49:51 GMT", 
-      "description": "", 
-      "email": "ludwig.thurfjell@gmail.com", 
-      "expires": "Wed, 13 Nov 2013 12:51:20 GMT", 
-      "id": 6, 
-      "last_seen": "Wed, 13 Nov 2013 11:51:20 GMT", 
-      "token": "b1007fb85cde8110c1bb17861d1fb3cc7f4aa429cf02bc1ca6", 
-      "username": "", 
-      "votesum": 0
-    }
-  }));
+
+  this.profile = new Profile();
 
   console.log(window.backendURL + '/u/' + uid +'/');
 
-  var self = this;
-  postJSON(window.backendURL + '/u/' + uid + '/', 'GET').done(function(response) {
-    console.log("This is blejz");
-    self.responseJSON(response);
-  }); 
+	$.ajax({
+		url: 'http://130.240.5.168:5000/u/' + uid + '/',
+		dataType: 'json',
+		success: function(data){
+			console.log(data.User);
+			self.profile.update(data.User);
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log("error");
+		}
+	});
+	
+	this.profile = new Profile({"username":"Dupr"});
+	
 }
