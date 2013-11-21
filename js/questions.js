7d0256eedf867c73daffc925b0ae93e1923835ce
Thurfjell <ludwig.thurfjell@gmail.com>
2013-11-21 12:15:22 +0100

    USA USA USA

--- js/questions.js
+++ js/questions.js
@@ -43,6 +43,7 @@ var qFilter = function(){
 	self.orderby = ko.observable(false);
 	self.filtername = ko.observable();
     self.orderByVotes = ko.observable(true);
+	
 	/*
 	
 	self.filterTest3 = ko.observable(false);
